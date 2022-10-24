9982ff0f53d35af14e0317b6d1d96c17989be426
yaphongkheng <hongkheng.yap@gmail.com>
2022-10-25 03:35:42 +0800

    add talk info

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,68 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import aclubMag from './assets/aclub_anime_magazine.jpg'
+  import makona from './assets/makona.png'
+  import sailormoon from './assets/Sailor_Senshi2.png'
+  import smcomics from './assets/sailormoon_comic_books.jpeg'
+  import animenano from './assets/animenano.png'
 </script>
 
 <main>
   <div>
     <h1>The Melancholy of Hong Kheng</h1>
     <p>About my own dying hobby of anime fandom in my younger days.</p>
+
   </div>
+</main>
+
+<main class="collage">
+  <div class="bg-collage">
+    <img class="overlap" style="top: 0; left :0;" src={sailormoon} alt="sailormoon" />
+    <img class="overlap" style="left: 0; bottom: 0;" src={aclubMag} alt="a-club" />
+    <img class="overlap" style="top: 0; left: 600px; right: 0;" width="600" src={smcomics} alt="sm comics" />
+  </div>
+</main>
+
+<main>
+  <h1>Early internet days</h1>
+  <div>
+    <p>Includes photos of skins of winamp </p>
+  </div>
+
+</main>
+
+<main>
+  <h1>Cable TV, somewhat faster internet</h1>
+</main>
+
+<main class="collage">
+  <img src={animenano} alt="anime nano" width="70%" />
+</main>
+
+<main>
+  <h2>Do get the resources from torrents site(s)</h2>
+</main>
+
+<main>
+  <h1>Notable shounen titles</h1>
+
+</main>
+
+<main>
+  clannad, shana, fate stay
+</main>
+
+<main>
+  shiroboko
+</main>
+
+<main>
+  <h1>why?</h1>
+  
+</main>
+
+<main>
+  <h1>Credits</h1>
+  <img width='200' src={makona} alt="makona" />
 </main>
 
 <style>
