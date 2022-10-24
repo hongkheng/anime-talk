6257bd6a81c716a0938909c2f84f97c741ebea57
Hong Kheng Yap <hongkheng.yap@gmail.com>
2022-10-24 17:20:22 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <h1>The Melancholy of Hong Kheng</h1>
+    <p>About my own dying hobby of anime fandom in my younger days.</p>
+  </div>
+</main>
+
+<style>
+
+</style>
